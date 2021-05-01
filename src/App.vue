436452f485a28baa564e9f83c56bbce89321f38f
mzz2020 <mingzhuzhan2020@163.com>
2021-05-01 20:04:02 +0800

    加入高德地图

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,12 @@
 </template>
 
 <style lang="scss">
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
