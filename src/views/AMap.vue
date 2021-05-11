6c6e4af09cce577983e4df7b246ba7c7ce928e67
mzz2020 <mingzhuzhan2020@163.com>
2021-05-11 23:52:54 +0800

    高德地图优化

--- src/views/AMap.vue
+++ src/views/AMap.vue
@@ -58,8 +58,8 @@ export default {
       .then(AMap => {
         // 地图实例配置
         amap.value = new AMap.Map('AMapRef', {
-          zoom: 10, // 级别
-          zooms: [4, 18], //设置地图级别范围
+          zoom: 19, // 级别
+          zooms: [4, 19], //设置地图级别范围
           viewMode: '3D', // 使用3D视图
           center: [116.397428, 39.90923] //中心点坐标
         })
